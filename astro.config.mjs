fed8284d3c788f63dadf35750c1af77eab6cc864
ATSYS <hola@atsys.co>
2024-06-30 20:20:43 -0500

    Added Netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import rename from 'astro-rename';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: 'https://atsys.co',
   output: 'static',
-  integrations: [tailwind(), react(), sitemap(), rename()]
+  integrations: [tailwind(), react(), sitemap(), rename()],
+  adapter: netlify()
 });
